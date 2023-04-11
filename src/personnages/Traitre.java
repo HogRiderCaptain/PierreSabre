5ae81cc8ba99bc076feedf950a61318968d7ad2f
Brandon Afonso <fnb4221a@ens.fsi>
2023-04-11 19:57:37 +0200

    Les traitres

---
+++ src/personnages/Traitre.java
@@ -0,0 +1,14 @@
+package personnages;
+
+public class Traitre extends Samourai {
+	private int niveauTraitrise = 0;
+	
+	public Traitre(String seigneur, String nom, String boisson, int argent) {
+		super(seigneur, nom, boisson, argent);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void raconner(Commercant commercant) {
+		
+	}
+}
